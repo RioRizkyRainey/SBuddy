636bf8751acd52509191c57c6e17404ac67408cf
Rio Rizky Rainey Ferbiansyah <rio.ferbiansyah@idntimes.com>
2024-12-06 14:46:43 +0700

    Initial Commit

---
+++ SBuddy/SBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SBuddyApp.swift
+//  SBuddy
+//
+//  Created by Rio Rizky Rainey Ferbiansyah on 06/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SBuddyTests/SBuddyTests.swift
@@ -0,0 +1,16 @@
+//
+//  SBuddyTests.swift
+//  SBuddyTests
+//
+//  Created by Rio Rizky Rainey Ferbiansyah on 06/12/24.
+//
+
+import Testing
+
+struct SBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
